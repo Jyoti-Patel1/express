07bacff4c25e9b0fb4d37cc28523cf9dea0c630d
kalpana patel <patelljyoti5@gmail.com>
2026-05-20 05:55:20 +0530

    adding 404 page not found

--- project airbnb/app.js
+++ project airbnb/app.js
@@ -17,6 +17,10 @@ app.use(express.urlencoded({ extended: true }));
 app.use(userRouter);
 app.use(hostRouter);
 
+app.use((req, res, next) => {
+  res.status(404).send("<h1> page not found</h1>");
+});
+
 const PORT = 8080;
 app.listen(PORT, () => {
   console.log(`server running on http://localhost:${PORT}`);
